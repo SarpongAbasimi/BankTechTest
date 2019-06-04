155fb9b1b09079d435f88e642b59858ba503d256
sarpong <abasimisarpong@rocketmail.com>
2019-06-04 18:24:20 +0100

    add transaction statement to Transactions class

--- lib/transactions.rb
+++ lib/transactions.rb
@@ -1,6 +1,26 @@
+require_relative 'statement'
 
 class Transactions
+  TRANSACTIONS_HEADERS = "date #{'   '}|| credit || debit || balance\r\n"
+
+  def initialize(statement = Statement.new)
+    @statement = statement
+  end
+  
   def make(credit, debit, balance)
-    credit
+    date = Time.new 
+    transaction_date = "#{date.year}/#{date.month}/#{date.day}"
+    @statement.history.push([transaction_date, credit, debit, balance])
+  end
+
+  def transaction_statement
+    print TRANSACTIONS_HEADERS
+    @statement.history.each do |each_amount|
+      if each_amount[2] == ""
+        print("#{each_amount[0]}|| #{each_amount[1]} #{'   '} ||       || #{each_amount[3]}\n")
+      else   
+        print("#{each_amount[0]}||        || #{each_amount[2]}    || #{each_amount[3]}\n")
+      end
+    end
   end
 end
