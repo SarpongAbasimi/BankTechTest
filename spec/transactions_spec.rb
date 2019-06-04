4449b290659eb2b884eb2963a5aa5d7395dfd55d
sarpong <abasimisarpong@rocketmail.com>
2019-06-04 18:30:26 +0100

    make method of transaction passes test

--- spec/transactions_spec.rb
+++ spec/transactions_spec.rb
@@ -1,16 +1,21 @@
 require 'transactions'
 
 RSpec.describe Transactions do
-  let(:transaction) { described_class.new }
-  let(:account) { double :account, credit: 10, balance: 20 , debit: ''}
-
+  let(:statement) { double :statement, history: [] }
+  let(:transaction) { described_class.new(statement) }
+  let(:account) { double :account, credit: 10, balance: 20 , debit: '' }
   describe '#make' do
     it 'gets passed amount from transaction' do
       credit = account.credit
       debit = account.debit
       balance = account.balance
-
-      expect(transaction.make(credit, debit, balance)).to eq(account.credit)
+      transaction.make(credit, debit, balance)
+      time = Time.new
+      allow(time).to receive(:year).and_return(2019)
+      allow(time).to receive(:month).and_return(06)
+      allow(time).to receive(:day).and_return(04)
+      transaction_date = "#{time.year}/#{time.month}/#{time.day}"
+      expect(statement.history).to include([transaction_date, credit, debit, balance])
     end
   end
 end
