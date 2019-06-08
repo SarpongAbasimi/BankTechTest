f68c920db6a96f571d073b513ad8b643fa9d81ea
sarpong <abasimisarpong@rocketmail.com>
2019-06-08 17:09:24 +0100

    Formated credit in account statement to have the two zeros at the end

--- lib/transactions.rb
+++ lib/transactions.rb
@@ -1,14 +1,14 @@
 require_relative 'statement'
 
 class Transactions
-  TRANSACTIONS_HEADERS = "date || credit || debit || balance\n"
+  TRANSACTIONS_HEADERS = "date || credit || debit || balance\n".freeze
 
   def initialize(statement = Statement.new)
     @statement = statement
   end
-  
+
   def make(credit, debit, balance)
-    time = Time.now.strftime("%d/%m/%Y")
+    time = Time.now.strftime('%d/%m/%Y')
     @statement.history.push([time, credit, debit, balance])
   end
 
@@ -20,13 +20,14 @@ class Transactions
   end
 
   private
+
   def credit_or_debit(each_amount)
-    if each_amount[2] == ""
-      print("#{each_amount[0]}|| #{each_amount[1]}||       || #{each_amount[3]}\n")
-    else   
-      print("#{each_amount[0]}||        || #{each_amount[2]}    || #{each_amount[3]}\n")
+    if each_amount[2] == ''
+      credit = '%.2f' % each_amount[1]
+      balance = '%.2f' % each_amount[3]
+      print "#{each_amount[0]}||#{credit}|| ||#{balance}\n"
+    else
+      print("#{each_amount[0]}|| || #{each_amount[2]} || #{balance}\n")
     end
   end
-
 end
-
--- spec/transactions_spec.rb
+++ spec/transactions_spec.rb
@@ -3,7 +3,7 @@ require 'transactions'
 RSpec.describe Transactions do
   let(:statement) { double :statement, history: [] }
   let(:transaction) { described_class.new(statement) }
-  let(:account) { double :account, credit: 10, balance: 20 , debit: '' }
+  let(:account) { double :account, credit: 10, balance: 20, debit: '' }
 
   describe '#make' do
     it 'gets passed amount from transaction' do
@@ -12,12 +12,8 @@ RSpec.describe Transactions do
       balance = account.balance
       transaction.make(credit, debit, balance)
 
-      time = Time.now.strftime("%d/%m/%Y")
-      
+      time = Time.now.strftime('%d/%m/%Y')
       expect(statement.history).to include([time, credit, debit, balance])
     end
   end
-  
 end
-
-print(Time.now.strftime("%d/%m/%Y"))
